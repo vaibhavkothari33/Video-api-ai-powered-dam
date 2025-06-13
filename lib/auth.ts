beb464b0e6e0a3b822b98a643c10e0ff9bcf50ec
vaibhavkothari33 <vaibhavkothari50@gmail.com>
2025-06-14 01:59:00 +0530

    Add ImageKit authentication and GitHub/Google providers to NextAuth

--- lib/auth.ts
+++ lib/auth.ts
@@ -3,9 +3,20 @@ import CredentialsProvider from "next-auth/providers/credentials";
 import { connectToDatabase } from "./db";
 import User from "@/models/User";
 import bcrypt from "bcryptjs";
+import GitHubProvider from "next-auth/providers/github";
+import GoogleProvider from "next-auth/providers/google";
 
 export const authOptions: NextAuthOptions = {
     providers: [
+        GitHubProvider({
+            clientId: process.env.GITHUB_ID!,
+            clientSecret: process.env.GITHUB_SECRET!,
+        }),
+
+        GoogleProvider({
+            clientId: process.env.GOOGLE_CLIENT_ID!,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+        }),
         CredentialsProvider({
             name: "Credentials",
             credentials: {
